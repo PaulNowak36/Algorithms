8098cb27ababbcdcc3ffe780f4c5f9d9659afdaf
PaulNowak36 <paul.nowak@algosup.com>
2023-05-24 19:54:48 +0200

    updated arrays

--- AlgorithmsTestProject/ArrayProblems.cs
+++ AlgorithmsTestProject/ArrayProblems.cs
@@ -1,53 +1,70 @@
+using Microsoft.VisualBasic;
+
 namespace AlgorithmsTestProject;
 
 public static class ArrayProblems
 {
     public static bool AreArraysEqual<T>(T[] xs, T[] ys)
     {
-        throw new NotImplementedException();
+        if (xs.Length != ys.Length) return false;
+
+        else if (xs.Equals(ys)) return true;
+
+        else return false;
     }
 
     public static void Swap<T>(T[] xs, int a, int b)
     {
+        var newValue = xs[a];
         xs[a] = xs[b];
-        xs[b] = xs[a];
+        xs[b] = newValue;
+
     }
 
     public static T FirstElement<T>(T[] xs)
     {
-        throw new NotImplementedException();
+        return xs[0];
     }
 
     public static T LastElement<T>(T[] xs)
     {
-        throw new NotImplementedException();
+        return xs[xs.Length - 1];
     }
 
     public static T MiddleElement<T>(T[] xs)
     {
-        throw new NotImplementedException();
+        
+        return xs[xs.Length / 2];
     }
 
     public static void Reverse<T>(T[] xs)
     {
-        throw new NotImplementedException();
+        Array.Reverse(xs);
+
     }
 
     public static int CountElement<T>(T[] xs, T element)
-    { 
-        throw new NotImplementedException();
+    {
+        return Count(xs, x => x.Equals(element));
     }
 
     public static string ToCommaDelimitedString<T>(T[] xs)
     {
-        throw new NotImplementedException();
+        string concat = string.Join(",", xs);
+        return concat;
     }
 
     // Bonus problems
 
     public static int Count<T>(T[] xs, Func<T, bool> predicate)
     {
-        throw new NotImplementedException();
+        var r = 0;
+        for (var i = 0; i < xs.Length; ++i)
+        {
+            if (predicate(xs[i]))
+                r++;
+        }
+        return r;
     }
 
     public static T Min<T>(T[] xs, Func<T, T, int> comparer)
